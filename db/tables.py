a3ce41c175afa7a98c7bdc2d7ab68176883e0e30
Pranav Manjapra <pranav.manjapra@gmail.com>
2026-08-12 19:29:44 -0700

    adding check constraints for subject.response and cell_count.population cols

--- db/tables.py
+++ db/tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import CheckConstraint, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -15,19 +15,23 @@ class Subject(Base):
     __tablename__ = "subject"
 
     id: Mapped[str] = mapped_column(primary_key=True)
-    project_id: Mapped[int] = mapped_column(ForeignKey("project.id"))
+    project_id: Mapped[str] = mapped_column(ForeignKey("project.id"))
     condition: Mapped[str]
     age: Mapped[int]
     sex: Mapped[str]
     treatment: Mapped[str]
-    response: Mapped[str]
+    response: Mapped[str | None]
+
+    __table_args__ = (
+        CheckConstraint("response IN ('yes', 'no')", name="check_subject_response"),
+    )
 
 
 class Sample(Base):
     __tablename__ = "sample"
 
     id: Mapped[str] = mapped_column(primary_key=True)
-    subject_id: Mapped[int] = mapped_column(ForeignKey("subject.id"))
+    subject_id: Mapped[str] = mapped_column(ForeignKey("subject.id"))
     sample_type: Mapped[str]
     time_from_treatment_start: Mapped[int]
 
@@ -38,3 +42,10 @@ class CellCount(Base):
     sample_id: Mapped[str] = mapped_column(ForeignKey("sample.id"), primary_key=True)
     population: Mapped[str] = mapped_column(primary_key=True)
     count: Mapped[int]
+
+    __table_args__ = (
+        CheckConstraint(
+            "population IN ('b_cell','cd8_t_cell','cd4_t_cell','nk_cell','monocyte')",
+            name="check_cell_count_population",
+        ),
+    )
